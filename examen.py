7bc620821347ee30ae12c188345b5fbea5df9e7d
dianamromeroc <101436143+dianamromeroc@users.noreply.github.com>
2022-03-15 13:17:52 -0500

    cambio total codigo

--- examen.py
+++ examen.py
@@ -1,8 +1,32 @@
-import numpy as np
-def mostrar_matriz(matriz):
-  for fila in matriz:
-    print(fila)
+nombres = []
+edades = []
 
-print(matriz)
+while True:
 
-matriz = np.array([[1,2,3,4,5],[3,5,6,4,9],[7,4,9,3,8],[12,15,23,17,14],[8,1,3,2,5]])
+    nombre = input('Nombre del estudiante: ')
+    if nombre == '*': 
+      break
+    edad = int(input('Ingrese edad del estudiante: '))
+    if edad > 0:
+      nombres.append(nombre)
+      edades.append(edad)
+    else:
+      print('Estudiante no valido (verifique edad)')
+  
+
+mayores = []
+edad_mayor = 0
+edad_mayor_ind = 0
+
+for j, edad in enumerate(edades):
+  if edad >= 18:
+    mayores.append(j)
+  if edad > edad_mayor:
+    edad_mayor = edad
+    edad_mayor_ind = j
+
+print('los estudiantes mayores de edad son:')
+for j in mayores:
+  print('*',nombres[j], edades[j])
+print('*'*20)
+print('Estudiante de mayor de edad:', nombres[edad_mayor_ind], '-', edades[edad_mayor_ind], 'años')
